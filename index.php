66a14f8dce532442cd00e10a50c1ddf325f87c8e
root <root@developer.devops.com>
2024-01-21 14:30:31 +0545

    myphp first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DevOps Project Page</title>
+</head>
+<body>
+
+    <?php
+        $name = "Anil Raj Rimal";
+    ?>
+
+    <h1>Welcome, <?php echo $name; ?>!</h1>
+    
+    <p>This page is created for a DevOps project.</p>
+
+</body>
+</html>
+
+
